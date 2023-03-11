2e78487d42d1f8a5049fb3bcbba817e9cdcee7b6
Herobaku <mammedovemin@gmail.com>
2023-03-11 05:22:30 +0400

    final layihe ver 1.8

--- js/slider.js
+++ js/slider.js
@@ -5,4 +5,10 @@ $(document).ready(()=>{
         autoplay: true,
         autoplaySpeed: 10000,
     })
+
+    $(".slickMobile").slick({
+        arrows: true,
+        // autoplay: true,
+        // autoplaySpeed: 10000,
+    })
 })
